bec03d70e4bfc853fdb592fed5ebdc47eef58f3e
timiduser <109868275+timiduser@users.noreply.github.com>
2023-04-19 09:27:05 -0500

    Delete DomainJoin.ps1

--- DomainJoin.ps1
+++
@@ -1,5 +0,0 @@
-$domain = "sonic.com"
-$password = "Child Branch Rock Comb Oil(" | ConvertTo-SecureString -asPlainText -Force
-$username = "$domain\tyler.schoneman" 
-$credential = New-Object System.Management.Automation.PSCredential($username,$password)
-Add-Computer -DomainName $domain -Credential $credential -restart
