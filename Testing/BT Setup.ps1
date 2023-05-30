888c8217203e261a71f0368b9a196558f36c8ade
timiduser <109868275+timiduser@users.noreply.github.com>
2023-05-30 11:17:45 -0500

    Added more installers and removed connectwise

--- Testing/BT Setup.ps1
+++ Testing/BT Setup.ps1
@@ -1,21 +1,9 @@
-# Run Script as Admin
-    if (Test-Path -Path "$($env:TEMP)\start.txt") {
-        Write-Output "Running as administrator!"
-    }
-    else {
-        New-Item "$($env:TEMP)\start.txt" -Force
+# Starts the script as Admin
         Start-Process powershell.exe "-File",('"{0}"' -f $MyInvocation.MyCommand.Path) -Verb RunAs
-        Exit
-    }
 # Allow Powershell Scripts to run
     Set-ExecutionPolicy RemoteSigned
 # Variables for script
-    $site = "https://www.dell.com/support/home/en-us"
     $active = 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp'
-    $CCN = HOSTNAME
-    $fielPath = "${PSScriptRoot}\MuellerImage\Snaggit\RegInfo.txt"
-    $vcRedistPath = "{your-path}\VC_redist.x64.exe"
-    $params = "/uninstall /quiet /norestart"
     $choice = Read-Host -Prompt 'Please enter 1 for Laptop or 2 for Desktop'
 # Functions to be called later on
 function elivatedRun {
@@ -25,7 +13,6 @@ function elivatedRun {
 function clearWindow {
     Clear-Host
 }
-
 function cleanUp {
 # Removing items installed by script. Need to move this to a scrip on its own and call after a system re-boot.
     elivatedRun
@@ -35,63 +22,74 @@ function cleanUp {
     Uninstall-Module -Name DellBIOSProvider -Force
     Start-Process "${PSScriptRoot}\CDistro\VC_redist.x64.exe" -Wait -ArgumentList "/q /uninstall /norestart"
 }
-function compSetting {
-    # Install Office
-    # Install Splashtop
-    # Install VNC
-
-    
+function compSetting {   
     # Set Activity Monitoring
     Write-Output "Setting up Activity Monitoring"
     Copy-Item -Path 'C:\Apps\Software\silentservermode-setup-usr-stl-$$FQsLUlwGXVgeVwcOXRoHDAJUFEoKCQAUUgNWCRMGCUhMDR1WfnF9bXFrfnpzYw==.exe' -Destination "C:\$active" -Force
     Clear-Host
-    # Connectwise Install
-    Write-Output "Installing Conectwise in the background"
-    Start-Process -FilePath 'C:\Apps\Software\Agent_Install.msi' -ArgumentList /qn /quiet
+    # Install SoftPhone
+    Write-Output "Installing Mitel Soft phone"
+    Start-Process -FilePath 'C:\Apps\Software\MicoLab\softphone.msi' -ArgumentList /qn UC_SERVER_HOSTNAME=c1vmas.buchanan.com UC_LANGUAGE=en
     Clear-Host
     # Annyconnect Install
     Write-Output "Installing Anyconnect VPN"
-    Start-Process  -FilePath 'C:\Apps\Software\anyconnect.msi' -ArgumentList /qn /quiet
+    Start-Process  -FilePath 'C:\Apps\Software\anyconnect.msi' -ArgumentList /qn /quiet -Wait
     Clear-Host
     Write-Output "Setting VPN profile"
     Copy-Item -Path 'C:\Apps\Software\AnyConnectProfile.xsd' -Destination "C:\ProgramData\Cisco\Cisco AnyConnect Secure Mobility Client\Profile\"
     Clear-Host
     # BMC Install
     Write-Output "Installing BMC"
-    Start-Process -FilePath 'C:\Apps\Software\Buchanan_Windows_Client_Install.exe'  -ArgumentList /silent /install
+    Start-Process -FilePath 'C:\Apps\Software\BMC.exe'  -ArgumentList /silent /install -Wait
     Clear-Host
     # Google Chrome Install
     Write-Output "Installing Google Chrome"
-    Start-Process 'C:\Apps\Software\ChromeStandaloneSetup64.exe'  -ArgumentList /silent /install
+    Start-Process 'C:\Apps\Software\ChromeStandaloneSetup64.exe'  -ArgumentList /silent /install -Wait
     Clear-Host
     # Dell Command Install
     Write-Output "Installing Dell Command"
-    Start-Process 'C:\Apps\Software\dell_command.exe'  -ArgumentList /silent /install
+    Start-Process 'C:\Apps\Software\dell_command.exe'  -ArgumentList /silent /install -Wait
     Clear-Host
     # Duo Health Install
     Write-Output "Installing Duo Health"
-    Start-Process -FilePath 'C:\Apps\Software\DuoDeviceHealth.msi' -ArgumentList /qn /quiet
+    Start-Process -FilePath 'C:\Apps\Software\Duo.msi' -ArgumentList /qn /quiet -Wait
     Clear-Host
     # Firefox Install
     Write-Output "Installing Firefox"
-    Start-Process 'C:\Apps\Software\Firefox Install.msi'  -ArgumentList /qn /quiet
+    Start-Process 'C:\Apps\Software\Firefox Install.msi'  -ArgumentList /qn /quiet -Wait
     Clear-Host
     # Greenshot Install
     Write-Output "Installing Greenshot"
-    Start-Process 'C:\Apps\Software\Greenshot.exe'  -ArgumentList /silent /install
+    Start-Process 'C:\Apps\Software\Greenshot.exe'  -ArgumentList /silent /install -Wait
     Clear-Host
+    # Install Office
+    Write-Output "Installing Office 365"
+    Start-Process 'C:\Apps\Software\Office\Greenshot.exe'  /configure configuration-Office365-x64.xml -Wait
+    # Install VNC
+    Write-Output "Installing VNC Viewer"
+    Start-Process 'C:\Apps\Software\Office\VNC.msi'  -ArgumentList /qn /quiet -Wait
+    # Install Splashtop
+    Write-Output "Installing Splashtop"
+    Start-Process 'C:\Apps\Software\Office\Splashtop.msi'  -ArgumentList /qn /quiet -Wait
 }
 function title {
     $host.UI.RawUI.WindowTitle = "$Title"
 }
 # Clears the screen from previous entries
     clearWindow
-# Does the needed pre setup needed for Dell BIOS Provider to run and install properly
-    $Title = 'Pre-Setup'
-    title
-    preSetup
-# Gets, installs, and imports Dell BIOS Provider to use the commands needed
-    $Title = 'Dell BIOS Setup'
-    title
-    bPSInstall
 # Main Code
+'''
+Computer boots up
+Runs on first startup command for powershell
+Script will run as admin
+Scipt just needs to install programs
+No domain join
+No name change
+Cannot install connectwise as of the name
+Things that need to be done after script
+    name change
+    connectwise
+    domain join
+    push S1
+
+'''
