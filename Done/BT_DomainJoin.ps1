43fde1c01cfc81d983f8fede3693b7731d2e46f3
timiduser <109868275+timiduser@users.noreply.github.com>
2023-06-02 14:50:46 -0500

    Added and fixed BT domain joins

--- Done/BT_DomainJoin.ps1
+++ Done/BT_DomainJoin.ps1
@@ -2,9 +2,9 @@
 Start-Process powershell.exe "-File",('"{0}"' -f $MyInvocation.MyCommand.Path) -Verb RunAs
 # Allow Powershell Scripts to run
 Set-ExecutionPolicy RemoteSigned
-# Domain unjoin
+# Domain join
 $domain = "buchanan.com"
 $password = "The program gave an error of 2023" | ConvertTo-SecureString -asPlainText -Force
 $username = "$domain\ts_admin" 
 $credential = New-Object System.Management.Automation.PSCredential($username,$password)
-Remove-Computer -UnjoinDomainCredential $credential -WorkgroupName "WORKGROUP" -Restart -Force
+Add-Computer -DomainName $domain -Credential $credential -restart
