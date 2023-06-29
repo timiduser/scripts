840bd29b1adf806bd358a628aab0af8ac3470b5a
timiduser <109868275+timiduser@users.noreply.github.com>
2023-06-29 16:46:24 -0500

    Changed domain name

--- Done/WISLESS_DomainJoin.ps1
+++ Done/WISLESS_DomainJoin.ps1
@@ -1,4 +1,4 @@
-$domain = "crossmark.internal.pvt"
+$domain = "western-inventory.com"
 $username = Read-Host -Prompt 'Please enter your username!' 
 $password = Read-Host -Prompt 'Please enter your password!' | ConvertTo-SecureString -asPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential($username,$password)
