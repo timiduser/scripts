4b61dba746c6765fb478c3fbfd36fa89538cbd84
timiduser <109868275+timiduser@users.noreply.github.com>
2023-06-29 16:45:37 -0500

    New domain join

---
+++ Done/WISLESS_DomainJoin.ps1
@@ -0,0 +1,5 @@
+$domain = "crossmark.internal.pvt"
+$username = Read-Host -Prompt 'Please enter your username!' 
+$password = Read-Host -Prompt 'Please enter your password!' | ConvertTo-SecureString -asPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential($username,$password)
+Add-Computer -DomainName $domain -Credential $credential -restart
